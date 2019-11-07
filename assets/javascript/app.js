d4425674d9ff3f772fa108ab93664d47bf498443
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-11-07 14:50:52 -0800

    pseudocode outline complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    // Start button that initiates game, when clicked opens questions screen
+    // Questions screen gives options for player to click, 4 clickable buttons
+
+
+    // Correct guesses
+    // Incorrect guesses increase when question wrong, or if player times out
+
+    // Timer for 20 seconds each question
+    // Timer for pause to view correct answer 
+
+    // 10 questions
+
+    // Win screen shows correct guesses, incorrect guesses
+    // Lose screen if player times out, or if player chooses incorrectly
+
+    // Reset game shows beginning of start button, correct guesses reset, incorrect guesses reset
+
+
+
+
+
+})
